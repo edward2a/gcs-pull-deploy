c029c639ed71efbf8bc3919c311bb1bcc11cc009
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-02-19 11:08:09 +0000

    some fixes and actually execute it

--- scripts/gcs_pull_deploy_install.sh
+++ scripts/gcs_pull_deploy_install.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 
 INSTALL_DIR=/opt/gcs_pd
 VAR_DATA=/var/gcs_pd
@@ -6,15 +5,24 @@ INIT_SCRIPT_DIR=/usr/local/lib/systemd/system
 CONFIG_FILE=${INSTALL_DIR}/config.shvar
 
 
-install() {
+install_gcs_pd() {
     mkdir -p ${INSTALL_DIR} ${VAR_DATA}
     chmod 750 ${INSTALL_DIR} ${VAR_DATA}
-    install -u root -g root -m 750 -t ${INSTALL_DIR} gcs_pull_deploy.sh
-    install -u root -g root -m 644 -t ${INIT_SCRIPT_DIR} gcs_pull_deploy.service
+    [ -d "${INIT_SCRIPT_DIR}" ] || mkdir -p ${INIT_SCRIPT_DIR}
+    install -o root -g root -m 750 -t ${INSTALL_DIR} gcs_pull_deploy.sh
+    install -o root -g root -m 644 -t ${INIT_SCRIPT_DIR} gcs-pull-deploy.service
     systemctl daemon-reload
-    systemctl enable gcs-pd
+    systemctl enable gcs-pull-deploy
 }
 
 configure() {
     echo "VAR_DATA=${VAR_DATA}" >> ${CONFIG_FILE}
 }
+
+main() {
+    install_gcs_pd
+    configure
+}
+
+# Execute
+main
