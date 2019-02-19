69b368bacd85983e775672ecee6464d3b2d6584b
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-02-19 14:52:56 +0000

    add note on source of the original code; switch tabs to spaces

--- test/app/hello.go
+++ test/app/hello.go
@@ -1,17 +1,22 @@
+/*
+ * Original code taken from https://gowebexamples.com/hello-world/
+ */
+
+
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "net/http"
   "log"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
     log.Printf("URL Requested: %s\n", r.URL.Path)
-	})
+  })
 
-	err := http.ListenAndServe(":80", nil)
+  err := http.ListenAndServe(":80", nil)
   if (err != nil) { log.Fatalln(err) }
 }
