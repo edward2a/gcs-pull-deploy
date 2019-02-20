46d23b9a0d30a5c6fc199fe679039f331b2c56cb
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-02-20 14:09:02 +0000

    update name

--- test/app/scripts/install.sh
+++ test/app/scripts/install.sh
@@ -8,7 +8,7 @@ mkdir ${INSTALL_DIR}
 chown root:root /opt/go-hello
 chmod 750 /opt/go-hello
 
-install -m 750 -o root -g root -t /opt/go-hello hello
+install -m 750 -o root -g root -t /opt/go-hello go-hello
 install -m 644 -o root -g root -D -t /usr/local/lib/systemd/system go-hello.service
 
 systemctl daemon-reload
